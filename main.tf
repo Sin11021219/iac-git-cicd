fc4bab23c32074751e57abae0c41b48f0e7850ed
Sin11021219 <sinya11021219@gmail.com>
2024-12-12 23:06:51 +0900

    Add files via upload

--- main.tf
+++ main.tf
@@ -20,3 +20,15 @@ provider "aws {
 }
 
 
+//復習2回目
+terraform {
+    backend "s3" {
+        bucket         = "aws-ecs-terraform-tfstate"
+        key            = "terraform.tfstate"
+        region         = "ap-northeast-1"
+        dynamodb_table = "aws-ecs-terraform-tfstate-locking"
+        encrypt        = true
+    }
+}
+
+
