079c4cf4aa6287b4277478ee0bb301d442ccbe27
Sin11021219 <sinya11021219@gmail.com>
2024-12-13 23:15:13 +0900

    Add files via upload

--- local.tf
+++ local.tf
@@ -4,3 +4,10 @@ locals {
     app    = "go-simple-server"
 }
 
+//復習2回目
+locals {
+    name   = replace(basename(path.cwd), "_", "-")
+    region = "ap-northeast-1"
+    app    = "go-simple-server"
+}
+
--- network.tf
+++ network.tf
@@ -17,3 +17,26 @@ module "vpc" {
   enable_nat_gateway   = true
 }
 
+//復習2回目
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = local.name
+  cidr = "10.0.0.0/16"
+
+  azs             = ["${local.name}a", "${local.name}c"]
+   public_sunets  = ["10.0.11.0/24", "10.0.12.0/24"]
+   private_sunets  = ["10.0.1.0/24", "10.0.2.0/24"]
+
+   public_subnet_names  = ["Public Subnet 1a", "Public Subnet 1c"]
+   private_subnet_names  = ["Private Subnet 1a", "Private Subnet 1c"]
+
+   enable_dns_hostnames = true
+   enable_dns_support   = true
+
+   enable_nat_subnets   = true
+
+
+   }
+
